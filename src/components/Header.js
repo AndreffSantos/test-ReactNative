e05a04f6d2a5a0bb5026cbfaf04f29b46297c391
Andre <andrefelipe_santos@outlook.com>
2023-02-07 20:24:32 -0300

    ✨ feat(implementa componente Header):

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+export function Header() {
+  <View>
+    <Text>Olá, bem-vindo</Text>
+
+    <TouchableOpacity>
+      Status
+    </TouchableOpacity>
+  </View>
+}
